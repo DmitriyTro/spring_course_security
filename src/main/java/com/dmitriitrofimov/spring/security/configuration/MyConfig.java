d793b8e2d3aa4192151aade0f52706d25557543a
Dmitrii Trofimov <probcf@gmail.com>
2021-08-22 13:19:28 +0300

    create views for authentication

--- src/main/java/com/dmitriitrofimov/spring/security/configuration/MyConfig.java
+++ src/main/java/com/dmitriitrofimov/spring/security/configuration/MyConfig.java
@@ -17,7 +17,7 @@ public class MyConfig {
 		InternalResourceViewResolver internalResourceViewResolver =
 				new InternalResourceViewResolver();
 
-		internalResourceViewResolver.setPrefix("/WEB-INF/view");
+		internalResourceViewResolver.setPrefix("/WEB-INF/view/");
 		internalResourceViewResolver.setSuffix(".jsp");
 
 		return  internalResourceViewResolver;
