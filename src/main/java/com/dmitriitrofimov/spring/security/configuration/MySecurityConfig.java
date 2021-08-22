8fa3e22b3b881c97514042efc6e064ff2d06bd4a
Dmitrii Trofimov <probcf@gmail.com>
2021-08-22 13:33:36 +0300

    add authorization for roles

--- src/main/java/com/dmitriitrofimov/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/dmitriitrofimov/spring/security/configuration/MySecurityConfig.java
@@ -1,6 +1,7 @@
 package com.dmitriitrofimov.spring.security.configuration;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -16,6 +17,15 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.inMemoryAuthentication().withUser(userBuilder.username("Zaur").password("Zaur").roles("EMPLOYEE"))
 		.withUser(userBuilder.username("Elena").password("Elena").roles("HR"))
 		.withUser(userBuilder.username("Ivan").password("Ivan").roles("MANAGER", "HR"));
+	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+				.antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+				.antMatchers("/hr_info/**").hasRole("HR")
+				.antMatchers("/manager_info/**").hasRole("MANAGER")
+				.and().formLogin().permitAll();
 	}
 }
